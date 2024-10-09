a9bef9e706b92278732ef435323bfbdafb2ecb59
alisq <ali.shamas.qadeer@gmail.com>
2024-10-09 12:46:17 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('h1#top').marquee({
+	speed: 150,
+	gap: 100,
+	delayBeforeStart: 0,
+	direction: 'left',
+	duplicated: true,
+	pauseOnHover: true
+});
+
+
+$('h2#bottom').marquee({
+	speed: 250,
+	gap: 100,
+	delayBeforeStart: 0,
+	direction: 'left',
+	pauseOnHover: true
+});
