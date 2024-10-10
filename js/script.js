f4c505d7f60408271f69fa5fd7831b172d6fc3f6
alisq <ali.shamas.qadeer@gmail.com>
2024-10-10 12:58:15 -0400

    added mobile.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 
 
+if ($(window).width() > 1000) {
+	$('#container').scroll(function(){
+		st = $("#container").scrollTop()/($(".interior").height()-$('#container').height())
+		$("h1#top").css("left",-200+(-2000*st)+"px")
+		$("h1#bottom").css("text-indent",-600+(500*st)+"px")
+		})
+} else {
+	$('h1#top').marquee({
+		speed: 100,
+		gap: 100,
+		delayBeforeStart: 0,
+		direction: 'left',
+		pauseOnHover: false
+	});
 
-$('#container').scroll(function(){
-st = $("#container").scrollTop()/($(".interior").height()-$('#container').height())
-$("h1#top").css("left",-200+(-2000*st)+"px")
-$("h1#bottom").css("text-indent",-600+(500*st)+"px")
-})
+	$('h1#bottom').marquee({
+		speed: 100,
+		gap: 100,
+		delayBeforeStart: 0,
+		direction: 'left',
+		pauseOnHover: false
+	});
+}
+
 
 
 
