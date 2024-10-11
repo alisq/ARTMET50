0a9f27d37aee200f8f897d2c45006d4dbcfaa771
alisq <ali.shamas.qadeer@gmail.com>
2024-10-11 08:43:38 -0400

    fixed screipt?

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 if ($(window).width() > 1000) {
 	$('#container').scroll(function(){
 		st = $("#container").scrollTop()/($(".interior").height()-$('#container').height())
-		$("h1#top").css("left",-200+(-2000*st)+"px")
+		a = -200+(-2000*st)+"px";
+		console.log(a)
+		$("h1#top").css("text-indent",a)
 		// $("h1#bottom").css("text-indent",-600+(500*st)+"px")
 		})
 } else {
